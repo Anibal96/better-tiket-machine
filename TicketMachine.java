f09f43366043fda1d3b6c68fa459d857837a0457
Anibal <anibaltascon@gmail.com>
2015-11-09 12:37:48 +0100

    Añadida posibilidad de descuento

--- TicketMachine.java
+++ TicketMachine.java
@@ -17,11 +17,13 @@ public class TicketMachine
     private int balance;
     // The total amount of money collected by this machine.
     private int total;
+    //Informa de si es una maquina con descuento.
+    private boolean descuento;
 
     /**
      * Create a machine that issues tickets of the given price.
      */
-    public TicketMachine(int cost)
+    public TicketMachine(int cost,boolean descuento)
     {
         price = cost;
         balance = 0;
@@ -102,16 +104,38 @@ public class TicketMachine
         return amountToRefund;
     }
     
-    public void emptyMachine()
+    /*
+    Metodo para vaciar la máquina.
+    */
+    public int emptyMachine()
     {
-        if (balance = 0){
-            int dinero;
-            dinero = total;
+        if (balance == 0) {
+            int recaudacion = total;
             total = 0;
-            return dinero;
+            return recaudacion; 
         }
-        else{
-            System.out.println(-1);
+        else {
+            return -1;
+        }
+    }
+    /**
+     * Imprime el ticket con descuento en caso de que la maquina tenga la opcion de true en descuento.
+     */
+    public void printTicketWithDiscount(){
+        if (descuento == true){
+            price = price-50;
+            // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
+
+            // Update the total collected with the price.
+            total = total + price;
+            // Reduce the balance by the prince.
+            balance = balance - price;
         }
     }
 }
